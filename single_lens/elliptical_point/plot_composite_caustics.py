2e59167dde3b00207aa810eab535d0a5f3659437
Alessandro Sonnenfeld <alesonny@gmail.com>
2022-07-12 00:26:56 +0200

    caustics of composite model

--- single_lens/elliptical_point/plot_composite_caustics.py
+++ single_lens/elliptical_point/plot_composite_caustics.py
@@ -9,14 +9,28 @@ rc('text', usetex=True)
 
 fsize = 18
 
-e_list = [0., 0.1, 0.2, 0.3]
+e_list = [0., 0.1, 0.2, 0.3, 0.4]
 ne = len(e_list)
 
 colseq = pylab.rcParams['axes.prop_cycle'].by_key()['color']
 
 fig, ax = pylab.subplots(figsize=(5.5, 5.5))
 
-pylab.subplots_adjust(left=0., right=1., bottom=0., top=1.)
+pylab.subplots_adjust(left=0.17, right=1., bottom=0.17, top=1.)
+
+xlim = (-0.95, 0.95)
+ylim = (-0.95, 0.95)
+
+"""
+xgrid = np.arange(xlim[0], xlim[1], 0.05)
+ygrid = np.arange(ylim[0], ylim[1], 0.05)
+
+for i in range(len(xgrid)):
+    ax.axvline(xgrid[i], color='grey', linewidth=0.5)
+
+for i in range(len(ygrid)):
+    ax.axhline(ygrid[i], color='grey', linewidth=0.5)
+"""
 
 for i in range(ne):
 
@@ -42,8 +56,24 @@ for i in range(ne):
 
 #ax.set_aspect(1.)
 
-pylab.legend(loc='lower right', fontsize=fsize)
+ax.tick_params(axis='both', which='both', top=True, right=True, labelsize=fsize, direction='in')#, width=1)
+ax.set_xlabel('$\\beta_x/\\theta_{\mathrm{Ein}}$', fontsize=fsize)
+ax.set_ylabel('$\\beta_y/\\theta_{\mathrm{Ein}}$', fontsize=fsize)
 
-#pylab.axis('off')
+ax.xaxis.set_major_locator(MultipleLocator(0.2))
+ax.xaxis.set_minor_locator(MultipleLocator(0.05))
+
+ax.yaxis.set_major_locator(MultipleLocator(0.2))
+ax.yaxis.set_minor_locator(MultipleLocator(0.05))
+
+ax.set_xlim(xlim[0], xlim[1])
+ax.set_ylim(ylim[0], ylim[1])
+
+pylab.arrow(-0.75, 0.78, 0.2, 0, length_includes_head=False, head_width=0.05, color='k')
+pylab.text(-0.8, 0.82, 'Major axis', fontsize=fsize)
+
+pylab.legend(loc='lower right', framealpha=1., fontsize=fsize)
+
+pylab.savefig('../../paper/composite_caustics.eps')
 pylab.show()
 
